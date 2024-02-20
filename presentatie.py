5e80dcb6fd50791a9d211bb9e65f60834e2967f3
Petra Molegraaf <148757046+LadyErebus13@users.noreply.github.com>
2024-02-20 11:10:09 +0100

    les 10 update

---
+++ presentatie.py
@@ -0,0 +1,7 @@
+def presenteer():
+    prijs_lijst = {"Aardbeien-ijs-totaal": 1000,
+             "Vanille-ijs-totaal": 2000,
+             "Chocolade-ijs-totaal": 1500,
+             "Waterijsjes-totaal": 750
+             }
+totaal = sum(presenteer())
